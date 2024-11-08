b95d2ac9abc6841f3027dae297a6f4a44261a179
Christian Lovetere <christianlovetere@usf.edu>
2024-11-07 23:55:55 -0500

    Update Temp_and_Humidity_Rev_2.ino
    fix TX address

--- TX (Arduino Nano)/Temp_and_Humidity_Rev_2/Temp_and_Humidity_Rev_2.ino
+++ TX (Arduino Nano)/Temp_and_Humidity_Rev_2/Temp_and_Humidity_Rev_2.ino
@@ -12,12 +12,18 @@ void setup() {
   Serial.begin(115200); //open serial for checking
 
   String listOfCommandsToSend[7] = {"AT\r\n", "AT+PARAMETER=7,9,4,7\r\n", "AT+NETWORKID=6\r\n",
-                                    "AT+ADDRESS=1\r\n", "AT+PARAMETER?\r\n", "AT+NETWORKID?\r\n",
+                                    "AT+ADDRESS=0\r\n", "AT+PARAMETER?\r\n", "AT+NETWORKID?\r\n",
                                     "AT+ADDRESS?\r\n"};
 
   for (int i = 0; i < 7; i++){
     Serial.print(listOfCommandsToSend[i]);
-    delay(250);
+    delay(125);
+    while (Serial.available()){
+      char c = Serial.read();
+      Serial.print(c);
+    }
+    delay(125);
+  Serial.flush();
   }
 }
 
