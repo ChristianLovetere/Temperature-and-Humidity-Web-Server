b61b9c06171d8deb119ef0c23be8df14d0e8a104
ChristianLovetere <christianlovetere@usf.edu>
2024-11-24 23:25:06 -0500

    Update Temp_and_Humidity_RX.ino

--- RX (ESP8266)/Temp_and_Humidity_RX/Temp_and_Humidity_RX.ino
+++ RX (ESP8266)/Temp_and_Humidity_RX/Temp_and_Humidity_RX.ino
@@ -1,11 +1,40 @@
 #define MILLIS_BETWEEN_READINGS 1000
 #define NUM_CONFIG_COMMANDS 7
 
-String buffer; //buffer to hold RF received info
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "Venue_slow";
+const char* password = "onmyhonor";
+
+ESP8266WebServer server(80);
+
+String buffer;
+String humString;
+String tempString;
+
+float humidity;
+float temperature;
+
+int dollarSignIdx;
+int percentIdx;
+int caratIdx;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200); //open serial for checking
+  Serial.begin(115200);
+
+  // Configure WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
+  // Configure web server
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Web server started");
 
   String listOfCommandsToSend[NUM_CONFIG_COMMANDS] = {"AT\r\n", "AT+PARAMETER=7,9,4,7\r\n", "AT+NETWORKID=6\r\n",
                                                       "AT+ADDRESS=1\r\n", "AT+PARAMETER?\r\n", "AT+NETWORKID?\r\n",
@@ -18,50 +47,44 @@ void setup() {
   }
 }
 
-String humString;
-String tempString;
-
-float humidity;
-float temperature;
-
-int dollarSignIdx; //index of the dollar sign delimiter
-int percentIdx; //index of the percent delimiter
-int caratIdx; //index of the carat delimiter
-
 void loop() {
   buffer = "";
-  while (Serial.available()){ //while Serial buffer has info,
-    char c = Serial.read(); //store byte as char
-    buffer += c; //append recent char to buffer
+  while (Serial.available()) {
+    char c = Serial.read();
+    buffer += c;
   }
-  buffer.replace("+ERR=2\r\n", ""); //remove extra error text
-  //Serial.print(buffer);
+  buffer.replace("+ERR=2\r\n", "");
 
-  dollarSignIdx = buffer.indexOf('$'); //get location of first delimiter
-  percentIdx = buffer.indexOf('%'); //get location of second delimiter
-  caratIdx = buffer.indexOf('^'); //get location of third delimiter
+  dollarSignIdx = buffer.indexOf('$');
+  percentIdx = buffer.indexOf('%');
+  caratIdx = buffer.indexOf('^');
 
-  if (newReadingsReceived())
-  {
+  if (newReadingsReceived()) {
     updateReadings();
   }
 
-  //Serial.print(humidity);
-  //Serial.print(" | ");
-  //Serial.println(temperature);
+  server.handleClient(); // Handle client requests
+
   delay(MILLIS_BETWEEN_READINGS);
 }
 
-bool newReadingsReceived()
-{
+bool newReadingsReceived() {
   return dollarSignIdx > -1 && percentIdx > -1 && caratIdx > -1;
 }
 
-void updateReadings()
-{
-  humString = buffer.substring(dollarSignIdx+1, percentIdx);
-  tempString = buffer.substring(percentIdx+1, caratIdx);
-  
+void updateReadings() {
+  humString = buffer.substring(dollarSignIdx + 1, percentIdx);
+  tempString = buffer.substring(percentIdx + 1, caratIdx);
+
   humidity = humString.toFloat();
   temperature = tempString.toFloat();
+}
+
+void handleRoot() {
+  String html = "<html><body>";
+  html += "<h1>Humidity and Temperature Readings</h1>";
+  html += "<p>Humidity: " + String(humidity) + "%</p>";
+  html += "<p>Temperature: " + String(temperature) + "C</p>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
 }
